6366dfbdb9ed7555ca72a4e10aa0c4e1cd70fe2d
Freeman75Add <revenjd13@gmail.com>
2024-06-23 14:28:18 -0700

    Create stronger.js

---
+++ stronger.js
@@ -0,0 +1,24 @@
+// no spaming under comments
+// test elements
+elements.Unknown = {
+    color: "#ffffff",
+    behavior: behaviors.wall,
+    category: "soilds",
+    viscosity: 0,
+    density: 0,
+};
+
+elements.water.behavior = behaviors.superfluid;
+
+
+elements.unknown2 = {
+    color: "#ffffff",
+    behavior: behaviors.agpowder,
+    category: "soilds",
+    viscosity: 1000,
+    density: 0,
+};
+
+
+
+//nothing else here
